cfa35d6a5828d5c5331c77e6ca53a95fd6d628a7
Josh Arnold <joshuaarnold22@gmail.com>
2015-09-08 20:16:05 -0400

    Ruby Models

--- spec/address_book.rb
+++ spec/address_book.rb
@@ -36,4 +36,22 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq 'joshuaarnold22@gmail.com'
     end
   end
+
+  context "#remove_entry" do
+    it "removed only one entry to the address book" do
+      book = AddressBook.new
+      book.add_entry('Josh Arnold', '423-676-8069', 'joshuaarnold22@gmail.com')
+
+      name = "Testing"
+      phone_number = "Another test"
+      email = "Last test"
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq 2
+
+      book.remove_entry('Josh Arnold', '423-676-8069', 'joshuaarnold22@gmail.com')
+
+      expect(book.entries.size).to eq 1
+    end
+  end
 end
